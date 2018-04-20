53474068fbcbf27fb100e628e4af328291ec2646
BAM-LAPTOP77\Ben <michaben@live.com>
2018-04-20 15:42:21 -0700

    Added Grenade class and Weapon superclass
    Edited Mace class to work with Weapon class

---
+++ Mace.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mace : Weapon
+{
+    public int maceDamage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Damage(collision.gameObject, maceDamage);
+    }
+
+}
