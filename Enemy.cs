25746098e52bac90a3b0af0517926f771e26d571
BAM-LAPTOP77\Ben <michaben@live.com>
2018-04-20 15:40:28 -0700

    Added files for the enemy

---
+++ Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public Text healthText;
+
+    public GameManager theGameManager;
+
+    public void takeDamage(int dmgToTake)
+    {
+        if (dmgToTake >= health)
+        {
+            theGameManager.GameOver(this.gameObject);
+        }
+        else
+        {
+            health = health - dmgToTake;
+            healthText.text = "Enemy health:\n" + health;
+        }
+    }
+}
---
+++ EnemyAgent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAgent : Agent
+{
+    Rigidbody2D enemyRGB2D;
+
+    private void Start()
+    {
+        enemyRGB2D = GetComponent<Rigidbody2D>();
+    }
+
+    public override void AgentReset()
+    {
+        // reset position and health
+    }
+}
