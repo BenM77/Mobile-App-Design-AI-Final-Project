6e4494774b91fb546c388138c35ea410a5df7a1a
Tristan Hildahl <thildahl20@my.whitworth.edu>
2018-05-03 16:18:30 -0700

    Update Spear

---
+++ SpearSprite.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearSprite : MonoBehaviour
+{
+    public GameObject spear;
+    public int throwForceX;
+
+    public void Throw(Character thrower)
+    {
+
+        int throwRight = 1;
+        if (!thrower.facingRight)
+            throwRight = -1;
+
+        GameObject newSpear = Instantiate(spear, thrower.transform.position + new Vector3((float)1 * throwRight, (float)1), Quaternion.identity);
+        newSpear.tag = "ThrownWeapon";
+        //newSpear.GetComponent<Animator>().SetTrigger("Thrown");
+        newSpear.GetComponent<Rigidbody2D>().AddForce(new Vector2(throwForceX * throwRight, 0));
+    }
+}
