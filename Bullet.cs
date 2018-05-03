c47fa81e9e156bfc24737ee5ed5050058f73f7d3
Tristan Hildahl <thildahl20@my.whitworth.edu>
2018-05-03 16:47:14 -0700

    Update Bullet.cs

--- Bullet.cs
+++ Bullet.cs
@@ -1,1 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Bullet : Weapon
+{
+    public int bulletDamage;  //Should be less than arrow damage
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject newHitObject = collision.gameObject;
+        if (newHitObject.tag == "Player" || newHitObject.tag == "Enemy")
+		{
+			newHitObject.SendMessage("takeDamage", bulletDamage);
+		}
+		
+	this.gameObject.SetActive(false);
+    }
+
+}
